c25a2508d543d1a5a36d879187e3664129f63629
clock <2567729201@qq.com>
2018-09-06 10:51:45 +0800

    tmp

---
+++ user_login/app/controllers/BMAuthController.scala
@@ -0,0 +1,97 @@
+package controllers
+
+import play.api.mvc._
+import io.circe.syntax._
+import akka.actor.ActorSystem
+import play.api.libs.circe.Circe
+import javax.inject.{Inject, Singleton}
+import com.pharbers.pattern.frame.PlayEntry
+import com.pharbers.jsonapi.model.RootObject
+import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
+
+@Singleton
+class BMAuthController @Inject()(implicit val cc: ControllerComponents, implicit val actorSystem: ActorSystem)
+        extends AbstractController(cc) with Circe with CirceJsonapiSupport {
+
+    import services._
+
+    def step(pkg: String, step: Int): Action[RootObject] = Action(circe.json[RootObject]) { implicit request =>
+        Ok(
+            (pkg, step) match {
+                case ("login", 0) => PlayEntry().excution(login()).asJson
+                case ("login", 1) => PlayEntry().excution(encryptToken()).asJson
+                case ("proposal", 0) => PlayEntry().excution(proposalLst()).asJson
+                case ("layout", 0) => PlayEntry().excution(proposalLst()).asJson
+                case (_, _) => throw new Exception("Bad Request for input")
+            }
+        )
+    }
+
+//    def proposalLst: Action[RootObject] = Action(circe.json[RootObject]) { implicit request =>
+//
+//        val in_data = formJsonapi[request](request.body)
+//        phLog(s"in_data = $in_data")
+//
+////        val reVal = entry.commonExcution(
+////            SequenceSteps(testStep(in_data) :: Nil, None)
+////        )
+//
+//        val result = new proposal()
+//        result.name = "关卡一"
+//        result.desc = "This is proposal first."
+//        result.scenario_id = "none"
+//        val result2 = new proposal()
+//        result2.name = "关卡二"
+//        result2.desc = "This is proposal second."
+//        result2.scenario_id = "123"
+//        val out_data = toJsonapi(result :: result2 :: Nil)
+//        phLog(s"out_data = $out_data")
+//        Ok(out_data.asJson)
+//    }
+//
+//    def layoutLst: Action[RootObject] = Action(circe.json[RootObject]) { implicit request =>
+//
+//        val in_data = formJsonapi[request](request.body)
+//        phLog(s"in_data = $in_data")
+//
+////        val reVal = entry.commonExcution(
+////            SequenceSteps(testStep(in_data) :: Nil, None)
+////        )
+//
+//        val hospdecision1 = new hospdecision()
+//        hospdecision1.component_name = "hospital-decision"
+//        val madecision1 = new madecision()
+//        madecision1.component_name = "manager-decision"
+//        val alldecision1 = new alldecision()
+//        alldecision1.component_name = "hospital-decision"
+//        alldecision1.hospitaldecison = Some(hospdecision1)
+//        alldecision1.managerdecision = Some(madecision1)
+//        val alldecision2 = new alldecision()
+//        alldecision2.component_name = "manager-decision"
+//        alldecision2.hospitaldecison = Some(hospdecision1)
+//        alldecision2.managerdecision = Some(madecision1)
+//        val out_data = toJsonapi(alldecision1 :: alldecision2 :: Nil)
+//
+//        phLog(s"out_data = $out_data")
+//        Ok(out_data.asJson)
+//        ???
+//    }
+
+    def test: Action[RootObject] = Action(circe.json[RootObject]) { implicit request =>
+//        val token = request.headers.get("Authorization").getOrElse("")
+//        println(s"*** token: $token")
+//
+//        val in_data = formJsonapi[request](request.body)
+//        phLog(s"in_data = $in_data")
+//
+////        val reVal = entry.commonExcution(
+////            SequenceSteps(testStep(in_data) :: Nil, None)
+////        )
+//
+//        val result = new auth()
+//        val out_data = toJsonapi(result.asInstanceOf[auth])
+//        phLog(s"out_data = $out_data")
+//        Ok(out_data.asJson)
+        ???
+    }
+}
---
+++ user_login/app/services/proposalLst.scala
@@ -0,0 +1,48 @@
+package services
+
+import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport._
+import com.pharbers.jsonapi.model
+import com.pharbers.models.user
+import com.pharbers.pattern.BrickRegistry
+import com.pharbers.pattern.frame._
+import com.pharbers.pattern.request.request
+import io.circe.syntax._
+import play.api.mvc.Request
+
+case class proposalLst()(implicit val request: Request[model.RootObject]) extends Brick {
+    override val brick_name: String = "get proposal list"
+
+    var request_data: request = null
+    var user_data: user = null
+    var token: String = ""
+
+    import com.pharbers.macros._
+    import com.pharbers.pattern.mongo.client_db_inst._
+    import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
+
+    override def prepare: Unit = request_data = formJsonapi[request](request.body)
+
+    override def exec: Unit = queryObject[user](request_data) match {
+        case Some(data) => user_data = data
+        case None => throw new Exception("Email is not registered or the password is not correct")
+    }
+
+    override def done: Option[String] = {
+        val bricks = BrickRegistry().registryRoute(api)
+        if (bricks.size - 1 <= cur_step)
+            None
+        else
+            Some(bricks(cur_step + 1))
+    }
+
+    override def forwardTo(next_brick: String): Unit = {
+        forward(next_brick)(api + (cur_step + 1)).post(request.body.asJson.noSpaces)
+    }
+
+    override def goback: model.RootObject = {
+        val out_data = toJsonapi(user_data)
+//        phLog(s"out_data = $out_data")
+        out_data
+    }
+
+}
